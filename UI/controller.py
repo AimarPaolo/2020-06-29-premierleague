1d4ed1655281f08b346487fe71243947bfc72f0f
Paolo Aimar <s297424@studenti.polito.it>
2024-07-01 21:01:43 +0200

    add files

---
+++ UI/controller.py
@@ -0,0 +1,33 @@
+import flet as ft
+
+
+class Controller:
+    def __init__(self, view, model):
+        # the view, with the graphical elements of the UI
+        self._view = view
+        # the model, which implements the logic of the program and holds the data
+        self._model = model
+
+        self._listYear = []
+        self._listCountry = []
+
+    def fillDDmin(self):
+        pass
+
+    def fillM1(self):
+        pass
+
+    def fillM2(self):
+        pass
+
+    def handle_graph(self, e):
+        pass
+
+
+
+    def handle_connesione_max(self, e):
+        pass
+
+
+    def handle_collegamento(self, e):
+        pass
